b85c925841887244432384a792d38f3b7868c4b2
webnerd <web_nerd@outlook.com>
2015-08-22 02:41:49 +0530

    added trainer components

---
+++ application/views/template/trainer_profile.php
@@ -0,0 +1,3 @@
+<?php $this->load->view(TEMPLATE . DIRECTORY_SEPARATOR . 'components/trainer_cover_image');?>
+<?php $this->load->view(TEMPLATE . DIRECTORY_SEPARATOR . 'components/trainer_review');?>
+<?php $this->load->view(TEMPLATE . DIRECTORY_SEPARATOR . 'components/trainer_details');?>
